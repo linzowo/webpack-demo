da9a3b517c93ce0dcca4c0f6cd48c16b02c51e44
linzowo <linzowo@outlook.com>
2019-08-26 15:58:47 +0800

    mint-ui初体验

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <h1>这是一个主组件</h1>
+
+        <mt-button type="primary">mui按钮</mt-button>
+        
         <router-link to="/account">用户</router-link>
         <router-link to="/goodslist">商品列表</router-link>
 
--- src/main.js
+++ src/main.js
@@ -6,7 +6,14 @@ import Vue from "vue";
 // 引入vue-router
 import VueRouter from "vue-router";
 
+// 引入mint-ui
+import MintUI from "mint-ui";
+// 引入样式
+import 'mint-ui/lib/style.css'
+
+// 将插件挂载到vue上
 Vue.use(VueRouter);
+Vue.use(MintUI);
 
 // 引入创建主组件
 import app from "./app.vue";
